e3e92166cb262db6ba3e916237ac7bb9c2d13efe
Jose Berrio <aldarisbm@gmail.com>
2024-04-22 22:40:24 -0400

    testing

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import spotipy
-
-from config import settings
+from mutagen.mp4 import MP4
 
 
 def main():
-    creds = spotipy.SpotifyClientCredentials(
-        client_id=settings.spotipy_client_id,
-        client_secret=settings.spotipy_client_secret
-    )
-    spotify = spotipy.Spotify(client_credentials_manager=creds)
-    playlist_id = input("Enter playlist ID: ")
-    playlist = spotify.playlist(playlist_id=playlist_id)
-    tracks = playlist['tracks']['items']
-    for track in tracks[:1]:
-        print(track)
+    mut = MP4('./test_files/Carmín.m4a')
+    print(mut['©alb'])
+
+    # mut['Title']
+    # print(mut.info.__dict__)
+
+
+# print(mut.keys())
+# print(mut.items())
+# print(mut.get('©nam'))
+# for i in mut.values():
+#     print(i)
+#     print('****************************')
 
 
 if __name__ == '__main__':
--- spotify/spotify.py
+++ spotify/spotify.py
@@ -18,3 +18,8 @@ class SpotifyAPI:
     def __init__(self):
         creds = spotipy.SpotifyClientCredentials(client_id=self.client_id, client_secret=self.client_secret)
         self._spotify_client = spotipy.Spotify(client_credentials_manager=creds)
+        playlist_id = input("Enter playlist ID: ")
+        playlist = self._spotify_client.playlist(playlist_id=playlist_id)
+        tracks = playlist['tracks']['items']
+        for track in tracks[:1]:
+            print(track)
